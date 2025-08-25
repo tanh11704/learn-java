7df5b5f3ec66db31712d27824afc9f7a815ccb3c
Trần Phước Anh <anhtp.22it@vku.udn.vn>
2025-08-25 17:53:19 +0700

    init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    jacoco
 }
 
 repositories {
@@ -28,7 +29,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
@@ -37,7 +38,27 @@ application {
     mainClass = "learn.java.App"
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
+
+tasks.test {
     useJUnitPlatform()
+    // Log test rõ ràng hơn trên CI
+    testLogging {
+        events("PASSED", "FAILED", "SKIPPED")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+    finalizedBy(tasks.jacocoTestReport) // luôn sinh report sau test
+}
+
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)   // Codecov cần file XML
+        html.required.set(true)  // để xem local
+        csv.required.set(false)
+    }
 }
